e2c290c39b5b414ed2c3899e1ce1b9ffb57de8f3
DavidVelez <felipvelez@gmail.com>
2016-03-18 01:54:10 -0500

    Update myrsa.py

--- myrsa.py
+++ myrsa.py
@@ -12,7 +12,8 @@
 #Solucion:
 
 
-#En el algoritmo se determinan 3 funciones de sustento que se complementan en f(RSA) es decir, que son fundamentales para crear y ejecutar la cuarta funcion que genera las llaves publica y privada del RSA.
+#En el algoritmo se determinan 3 funciones de sustento que se complementan en f(RSA) es decir, que son
+#fundamentales para crear y ejecutar la cuarta funcion que genera las llaves publica y privada del RSA.
 
 import random
 import sys
@@ -21,7 +22,9 @@ import sys
 
 #Estrategia:
 #Se utiliza el algoritmo de Euclides.
-#El algoritmo se centra basicamente en dividir el numero mayor a entre el numero menor b, mientras diferente de 0 y hasta que el resto de a/b sea 0, dado el caso de que a<b se aplica intercambio de valores a las variables, (funciona como un tipo de auxiliar que reemplaza valores).
+#El algoritmo se centra basicamente en dividir el numero mayor a entre el numero menor b, mientras diferente 
+#de 0 y hasta que el resto de a/b sea 0, dado el caso de que a<b se aplica intercambio de valores a las variables,
+#(funciona como un tipo de auxiliar que reemplaza valores).
 #Devuelve el maximo comun divisor de a y b.
 
 def Emcd(a, b):
@@ -70,17 +73,22 @@ def prime_num(num):
 # Funcion Claves RSA
 
 #Estrategia:
-#En primera instancia se confirma que p y q son numeros primos, de lo contrario se genera un tipo de break que me expulsa de la funcion.
-#Si los numeros cumplen la condicion son enviados a la siguiente etapa donde se genera N equivalente a (p*q) y en su efecto se genera el algoritmo de euler.
+#En primera instancia se confirma que p y q son numeros primos, de lo contrario se genera un tipo de break que me 
+#expulsa de la funcion.
+#Si los numeros cumplen la condicion son enviados a la siguiente etapa donde se genera N equivalente a (p*q) y en 
+#su efecto se genera el algoritmo de euler.
 #Para hallar e correspondiente a la llave publicaexisten dos condiciones basicas:[1 < e < phi] y coprimo con phi.
-#Por ello se crea una tupla que va desde 2 hasta phi sin contar phi, se implementa el modulo random.randrange que me genera una lista de numeros aleatoriamente con el rango y argumentos dados. Para que el modulo no se base en el tiempo para generar los numeros se utiliza random.seed(1) que define una instancia desde la cual siempre va actualizarce.## El modulo requiere que se importe al file random: import random.De otro modo el programa encuentra date flotante que genera error.
-#Para hallar d correspondiente a la clave privada se utiliza de igual manera el modulo random.randrange
+#Por ello se crea una tupla que va desde 2 hasta phi sin contar phi, se implementa el modulo random.randrange que
+#me genera una lista de numeros aleatoriamente con el rango y argumentos dados. Para que el modulo no se base en el
+#tiempo para generar los numeros se utiliza random.seed(1) que define una instancia desde la cual siempre va actualizarce.
+## El modulo requiere que se importe al file random: import random.De otro modo el programa encuentra date flotante 
+#que genera error.Para hallar d correspondiente a la clave privada se utiliza de igual manera el modulo random.randrange
 
 #Se utiliza la Funcion Numeros relativos para buscar en la lista 'e' el ultimo numero que sea coprimo con phi.
 
-# 'd' se encuentra multiplicando una lista de numeros * e, y si al dividir el producto entre phi, el residuo
-#  es 1, encotraremos una lista de posibles 'd'. Por tal razon se crea un ciclo en el que 'd' es una lista que va desde 0 hasta
-# phi para hallar el numero que cumple con la condicion.
+#'d' se encuentra multiplicando una lista de numeros * e, y si al dividir el producto entre phi, el residuo
+#es 1, encotraremos una lista de posibles 'd'. Por tal razon se crea un ciclo en el que 'd' es una lista que 
+#va desde 0 hasta phi*2 para hallar el d de la lista que me cumpla la condicion.
 
 def rsa(p,q):
         
@@ -99,19 +107,19 @@ def rsa(p,q):
 		e=random.randrange(2,phi)
 	     	if coprime_num (e,phi) == 1:
 			return e	
-		d=random.randrange(phi)
+		d=random.randrange(phi*2)
 		if e * d % phi == 1:
 			return d	
 		print "Done..."
 		print "LLave publica: ","e= ",e,',',"N= ",N #salida de llave publica
-		print "LLave privada: ","d= ",d,',',"N= ",N #salida de llave publica
+		print "LLave privada: ","d= ",d,',',"N= ",N #salida de llave privada
 
 	
 
 v=int(input("Ingrese el numero primo para p: "))#entrada de valor p
 u=int(input("Ingrese el numero primo para q: "))#entrada de valor q
 
-val=rsa(v,u)#evaluo de entradas
+val=rsa(v,u)#evaluacion de entradas
 print val
 
 
